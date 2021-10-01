719c216a33344538c8677bc49d948135df71deaa
BR7RKR <leomyts@gmail.com>
2021-10-01 15:31:32 +0300

    Lab_8_1

---
+++ Labs/Lab_8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lab_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная работа номер 8");
+            Console.WriteLine("Нажмите на клавишу 1-6 для перехода к нужному заданию");
+            //Подключаю обработку клавиш
+            var key = Console.ReadKey().Key;
+            switch (key)
+            {
+                case ConsoleKey.D1:
+
+
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
