33bcf49554053c781b899ed3943963f39a84bd35
BR7RKR <leomyts@gmail.com>
2021-10-13 17:59:54 +0300

    Lab_12_1

--- Labs/Lab_12/Program.cs
+++ Labs/Lab_12/Program.cs
@@ -5,7 +5,6 @@ namespace Lab_12
     class Program
     {
         private static WrongNumberException WrongNumberEx = new WrongNumberException("WrongNumberFormat");
-        private static string[] days = new string[] { "null", "первое", "второе", "третье", "четвертое", "пятое", "шестое", "седьмое", "восьмое", "девятое", "десятое", "одиннадцатое", "двенадцатое", "тринадцатое", "четырнадцатое", "пятнадцотое", "шестнадцатое", "семнадцатое", "восемнадцатое", "девятнадцатое", "двадцатое"};
 
         static void Main(string[] args)
         {
@@ -18,30 +17,20 @@ namespace Lab_12
                 case ConsoleKey.D1:
 
                     Console.Write(" - задание \n");
+                    string[] days = new string[] { "null", "первое", "второе", "третье", "четвертое", "пятое", "шестое", "седьмое", "восьмое", "девятое", "десятое", "одиннадцатое", "двенадцатое", "тринадцатое", "четырнадцатое", "пятнадцотое", "шестнадцатое", "семнадцатое", "восемнадцатое", "девятнадцатое", "двадцатое", "двадцать первое", "двадцать второе", "двадцать третье", "двадцать четвертое", "двадцать пятое", "двадцать шестое", "двадцать седьмое", "двадцать восьмое", "двадцать девятое", "тридцатое", "тридцать первое" };
+                    string[] months = new string[] { "null", "января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря" };
                     Console.Write("Введите номер дня [1;31] - ");
-                    bool isA = ulong.TryParse(Console.ReadLine(), out ulong A);
+                    bool isDay = byte.TryParse(Console.ReadLine(), out byte day);
                     Console.Write("Введите номер месяца [1;12] - ");
-                    bool isB = ulong.TryParse(Console.ReadLine(), out ulong B);
+                    bool isMonth = byte.TryParse(Console.ReadLine(), out byte month);
                     //вызываю собственные ошибки при нарушении правил ввода 
-                    if (isA == false || isB == false)
+                    if (isDay == false || isMonth == false || day > 31 || day < 1 || month > 12 || month < 1)
                     {
                         throw WrongNumberEx;
                     }
 
-                    if (A != B)//проверяю неравенство
-                    {
-                        temp = Math.Max(A, B);//заношу максимальное число в буферную переменную
-                        A = temp;
-                        B = temp;
-                    }
-                    else if (A == B)//проверяю равенство
-                    {
-                        A = 0;
-                        B = 0;
-                    }
-
                     Console.ForegroundColor = ConsoleColor.Green;//меняю цвет текста
-                    Console.WriteLine($"Новые значения A - {A} B - {B}");
+                    Console.WriteLine($"{days[day]} {months[month]}");
                     Console.ResetColor(); // сбрасываю цвета консоли
 
                     Console.BackgroundColor = ConsoleColor.Blue;  //меняю цвет фона текста
@@ -51,7 +40,34 @@ namespace Lab_12
                     Console.ReadLine(); // запрашиваю ввод
                     Console.Clear(); // очищаю консоль
                     Main(args); // перезапускаю программу
+                    break;
 
+                case ConsoleKey.D2:
+
+                    Console.Write(" - задание \n");
+                    string[] compasDir = new string[] { "С", "З", "Ю", "В"};
+                    //string[] months = new string[] { "null", "января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря" };
+                    Console.Write("Введите исходное направление робота - ");
+                    string start = Console.ReadLine();
+                    Console.Write("Введите команду - ");
+                    string command = Console.ReadLine();
+
+                    if (compasDir.Any())//вызываю собственные ошибки при нарушении правил ввода
+                    {
+                        throw WrongNumberEx;
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.Green;//меняю цвет текста
+                    Console.WriteLine($"{days[day]} {months[month]}");
+                    Console.ResetColor(); // сбрасываю цвета консоли
+
+                    Console.BackgroundColor = ConsoleColor.Blue;  //меняю цвет фона текста
+                    Console.ForegroundColor = ConsoleColor.Black; // меняю цвет фона текста
+                    Console.WriteLine("Нажмите ENTER для продолжения");
+                    Console.ResetColor(); //сбрасываю цвета консоли
+                    Console.ReadLine(); // запрашиваю ввод
+                    Console.Clear(); // очищаю консоль
+                    Main(args); // перезапускаю программу
                     break;
 
                 default:
