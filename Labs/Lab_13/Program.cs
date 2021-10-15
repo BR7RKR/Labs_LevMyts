50ea1df764942b94e3b1b90691387154658163cf
BR7RKR <leomyts@gmail.com>
2021-10-15 23:04:29 +0300

    Lab_13_0.1

---
+++ Labs/Lab_13/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Lab_13
+{
+    class Program
+    {
+        private static WrongNumberException WrongNumberEx = new WrongNumberException("WrongNumberFormat");
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная работа номер 12");
+            Console.WriteLine("Введите цифру от (1-5) для выбора задачи: ");
+
+            var key = Console.ReadKey().Key;
+            switch (key)
+            {
+                case ConsoleKey.D1:
+
+                    Console.Write(" - задание \n");
+                    Console.Write("Введите цену одного кг конфет - ");
+                    bool is1Price = double.TryParse(Console.ReadLine(), out double price);
+                    //вызываю собственные ошибки при нарушении правил ввода 
+                    if (is1Price == false || price <= 0)
+                    {
+                        throw WrongNumberEx;
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.Green;//меняю цвет текста
+                    for (double i = 0.1f; i <= 1; i += 0.1f)
+                    {
+                        Console.WriteLine($"{price * i}");
+                    }
+                    Console.ResetColor(); // сбрасываю цвета консоли
+
+                    Console.BackgroundColor = ConsoleColor.Blue;  //меняю цвет фона текста
+                    Console.ForegroundColor = ConsoleColor.Black; // меняю цвет фона текста
+                    Console.WriteLine("Нажмите ENTER для продолжения");
+                    Console.ResetColor(); //сбрасываю цвета консоли
+                    Console.ReadLine(); // запрашиваю ввод
+                    Console.Clear(); // очищаю консоль
+                    Main(args); // перезапускаю программу
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Labs/Lab_13/WrongNumberException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_13
+{
+    //Создаю новый класс ошибки и наследую его от класса Exception
+    class WrongNumberException : Exception
+    {
+        //Создаю конструктор класса и наследую его от одного из конструкторов класса Exception
+        public WrongNumberException(string message) : base(message){}
+    }
+}
