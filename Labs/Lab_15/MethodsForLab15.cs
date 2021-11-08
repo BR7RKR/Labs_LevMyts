9015b4ffadcfdfc9ce40807febb44067986adcec
BR7RKR <leomyts5@gmail.com>
2021-11-08 15:46:51 +0300

    Lab_15_final

---
+++ Labs/Lab_15/MethodsForLab15.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_15
+{
+    public static class MethodsForLab15
+    {
+        //Сделал метод и 2 перегрузки к нему, чтобы принимать любые типы вещественных чисел
+        public static double Power3A(in double inputNumber)
+        {
+            double outputNumber = inputNumber * inputNumber * inputNumber;
+            return outputNumber;
+        }
+        public static float Power3A(in float inputNumber)
+        {
+            float outputNumber = inputNumber * inputNumber * inputNumber;
+            return outputNumber;
+        }
+        public static decimal Power3A(in decimal inputNumber)
+        {
+            decimal outputNumber = inputNumber * inputNumber * inputNumber;
+            return outputNumber;
+        }
+
+        //Сделал метод и 2 перегрузки к нему, чтобы принимать любые типы вещественных чисел
+        public static int Sign(in double inputNumber)
+        {
+            if (inputNumber < 0)
+            {
+                return -1;
+            }
+            else if (inputNumber > 0 )
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public static int Sign(in decimal inputNumber)
+        {
+            if (inputNumber < 0)
+            {
+                return -1;
+            }
+            else if (inputNumber > 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public static int Sign(in float inputNumber)
+        {
+            if (inputNumber < 0)
+            {
+                return -1;
+            }
+            else if (inputNumber > 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        //Сделал метод и 1 перегрузку к нему, чтобы принимать разные типы вещественных чисел
+        public static double RingS(in double radius1, in double radius2)
+        {
+            var areaSmall = Math.PI * (Math.Min(radius1, radius2) * Math.Min(radius1, radius2));
+            var areaBig = Math.PI * (Math.Max(radius1, radius2) * Math.Max(radius1, radius2));
+            return areaBig - areaSmall;
+        }
+        public static double RingS(in float radius1, in float radius2)
+        {
+            var areaSmall = Math.PI * (Math.Min(radius1, radius2) * Math.Min(radius1, radius2));
+            var areaBig = Math.PI * (Math.Max(radius1, radius2) * Math.Max(radius1, radius2));
+            return areaBig - areaSmall;
+        }
+
+        //Сделал метод и 2 перегрузки к нему, чтобы принимать любые типы вещественных чисел
+        public static byte Quarter(in float x, in float y)
+        {
+            if (x > 0 && y > 0)
+            {
+                return 1;
+            }
+            else if (x > 0 && y < 0)
+            {
+                return 4;
+            }
+            else if (x < 0 && y < 0)
+            {
+                return 3;
+            }
+            else 
+            {
+                return 2;
+            }
+        }
+        public static byte Quarter(in double x, in double y)
+        {
+            if (x > 0 && y > 0)
+            {
+                return 1;
+            }
+            else if (x > 0 && y < 0)
+            {
+                return 4;
+            }
+            else if (x < 0 && y < 0)
+            {
+                return 3;
+            }
+            else
+            {
+                return 2;
+            }
+        }
+        public static byte Quarter(in decimal x, in decimal y)
+        {
+            if (x > 0 && y > 0)
+            {
+                return 1;
+            }
+            else if (x > 0 && y < 0)
+            {
+                return 4;
+            }
+            else if (x < 0 && y < 0)
+            {
+                return 3;
+            }
+            else
+            {
+                return 2;
+            }
+        }
+
+        
+        public static double Fact2(ulong number)
+        {
+            ulong res;
+            if (number % 2 != 0)
+            {
+                res = 1;
+                for (ulong i = 3; i <= number; i += 2)
+                {
+                    res *= i;
+                }
+            }
+            else
+            {
+                res = 2;
+                for (ulong i = 4; i <= number; i += 2)
+                {
+                    res *= i;
+                }
+            }
+            return res;
+        }
+    }
+}
--- Labs/Lab_15/Program.cs
+++ Labs/Lab_15/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Lab_13
+namespace Lab_15
 {
     class Program
     {
@@ -8,28 +8,40 @@ namespace Lab_13
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Лабораторная работа номер 13");
+            Console.WriteLine("Лабораторная работа номер 15");
             Console.WriteLine("Введите цифру от (1-5) для выбора задачи: ");
 
             var key = Console.ReadKey().Key;
             switch (key)
             {
+
                 case ConsoleKey.D1:
 
                     Console.Write(" - задание \n");
-                    Console.Write("Введите цену одного кг конфет - ");
-                    bool is1Price = float.TryParse(Console.ReadLine(), out float price);
+                    Console.Write("Введите первое число - ");
+                    bool isNumber1 = double.TryParse(Console.ReadLine(), out double number1);
+                    Console.Write("Введите второе число - ");
+                    bool isNumber2 = float.TryParse(Console.ReadLine(), out float number2);
+                    Console.Write("Введите третье число - ");
+                    bool isNumber3 = decimal.TryParse(Console.ReadLine(), out decimal number3);
+                    Console.Write("Введите четвертое число - ");
+                    bool isNumber4 = double.TryParse(Console.ReadLine(), out double number4);
+                    Console.Write("Введите пятое число - ");
+                    bool isNumber5 = double.TryParse(Console.ReadLine(), out double number5);
+
                     //вызываю собственные ошибки при нарушении правил ввода 
-                    if (is1Price == false || price <= 0)
+                    if (isNumber1 == false || isNumber2 == false || isNumber3 == false || isNumber4 == false || isNumber5 == false)
                     {
                         throw WrongNumberEx;
                     }
 
                     Console.ForegroundColor = ConsoleColor.Green;//меняю цвет текста
-                    for (float i = 0.1f; i <= 1.1; i += 0.1f)
-                    {
-                        Console.WriteLine($"{Math.Round(price * Math.Round(i, 1), 2)}");
-                    }
+                    Console.WriteLine("Третьи степени всех пяти чисел: ");
+                    Console.WriteLine(MethodsForLab15.Power3A(number1));
+                    Console.WriteLine(MethodsForLab15.Power3A(number2));
+                    Console.WriteLine(MethodsForLab15.Power3A(number3));
+                    Console.WriteLine(MethodsForLab15.Power3A(number4));
+                    Console.WriteLine(MethodsForLab15.Power3A(number5));
                     Console.ResetColor(); // сбрасываю цвета консоли
 
                     Console.BackgroundColor = ConsoleColor.Blue;  //меняю цвет фона текста
@@ -44,24 +56,19 @@ namespace Lab_13
                 case ConsoleKey.D2:
 
                     Console.Write(" - задание \n");
-                    Console.Write("Введите кол-во итераций N - ");
-                    bool isN = int.TryParse(Console.ReadLine(), out int N);
+                    Console.Write("Введите первое число - ");
+                    bool isNumberA = double.TryParse(Console.ReadLine(), out double numberA);
+                    Console.Write("Введите второе число - ");
+                    bool isNumberB = float.TryParse(Console.ReadLine(), out float numberB);
+
                     //вызываю собственные ошибки при нарушении правил ввода 
-                    if (isN == false || N <= 0)
+                    if (isNumberA == false || isNumberB == false)
                     {
                         throw WrongNumberEx;
                     }
 
-                    double res = 1.1;
-                    int k = 0;
-                    for (double i = 1.2; k < N-1; i+=0.1)
-                    {
-                        k += 1;
-                        res *= Math.Round(i, 1);
-                    }
-
                     Console.ForegroundColor = ConsoleColor.Green;//меняю цвет текста
-                    Console.WriteLine($"Ответ: {res}");
+                    Console.WriteLine($"Сумма двух возвращаемых значений = {MethodsForLab15.Sign(numberA) + MethodsForLab15.Sign(numberB)}");
                     Console.ResetColor(); // сбрасываю цвета консоли
 
                     Console.BackgroundColor = ConsoleColor.Blue;  //меняю цвет фона текста
@@ -76,24 +83,32 @@ namespace Lab_13
                 case ConsoleKey.D3:
 
                     Console.Write(" - задание \n");
-                    Console.Write("Введите число N - ");
-                    bool isNb = ulong.TryParse(Console.ReadLine(), out ulong n);
+
+                    Console.Write("Введите внутренний радиус - ");
+                    bool isRS1 = double.TryParse(Console.ReadLine(), out double rS1);
+                    Console.Write("Введите внешний радиус - ");
+                    bool isRB1 = double.TryParse(Console.ReadLine(), out double rB1);
+
+                    Console.Write("Введите внутренний радиус - ");
+                    bool isRS2 = double.TryParse(Console.ReadLine(), out double rS2);
+                    Console.Write("Введите внешний радиус - ");
+                    bool isRB2 = double.TryParse(Console.ReadLine(), out double rB2);
+
+                    Console.Write("Введите внутренний радиус - ");
+                    bool isRS3 = float.TryParse(Console.ReadLine(), out float rS3);
+                    Console.Write("Введите внешний радиус - ");
+                    bool isRB3 = float.TryParse(Console.ReadLine(), out float rB3);
+
                     //вызываю собственные ошибки при нарушении правил ввода 
-                    if (isNb == false)
+                    if (isRS1 == false || isRS2 == false || isRS3 == false || isRB1 == false || isRB2 == false || isRB3 == false || rS1 > rB1 || rS2 > rB2 || rS3 > rB3)
                     {
                         throw WrongNumberEx;
                     }
-                    Console.ForegroundColor = ConsoleColor.Blue;//меняю цвет текста
-                    ulong result = 0;
-                    for (ulong i = 1; i <= (2*n - 1); i+=2)
-                    {
-                        Console.WriteLine(i);
-                        result += i;
-                    }
-                    Console.ResetColor(); // сбрасываю цвета консоли
 
                     Console.ForegroundColor = ConsoleColor.Green;//меняю цвет текста
-                    Console.WriteLine($"N^2 {result}");
+                    Console.WriteLine($"Площадь первого кольца = {MethodsForLab15.RingS(rS1, rB1)}");
+                    Console.WriteLine($"Площадь второго кольца = {MethodsForLab15.RingS(rS2, rB2)}");
+                    Console.WriteLine($"Площадь третьего кольца = {MethodsForLab15.RingS(rS3, rB3)}");
                     Console.ResetColor(); // сбрасываю цвета консоли
 
                     Console.BackgroundColor = ConsoleColor.Blue;  //меняю цвет фона текста
@@ -108,31 +123,35 @@ namespace Lab_13
                 case ConsoleKey.D4:
 
                     Console.Write(" - задание \n");
-                    Console.Write("Введите число A - ");
-                    bool isNmbrA = double.TryParse(Console.ReadLine(), out double A);
-                    Console.Write("Введите число N - ");
-                    bool isNmbr = ulong.TryParse(Console.ReadLine(), out ulong _n);
-                    //вызываю собственные ошибки при нарушении правил ввода 
-                    if (isNmbr == false || isNmbrA == false)
+
+                    Console.WriteLine("Введите координаты первой точки: ");
+                    Console.Write("X - ");
+                    bool isX1_ = double.TryParse(Console.ReadLine(), out double x1_);
+                    Console.Write("Y - ");
+                    bool isY1_ = double.TryParse(Console.ReadLine(), out double y1_);
+
+                    Console.WriteLine("Введите координаты второй точки: ");
+                    Console.Write("X - ");
+                    bool isX2_ = float.TryParse(Console.ReadLine(), out float x2_);
+                    Console.Write("Y - ");
+                    bool isY2_ = float.TryParse(Console.ReadLine(), out float y2_);
+
+                    Console.WriteLine("Введите координаты третьей точки: ");
+                    Console.Write("X - ");
+                    bool isX3_ = decimal.TryParse(Console.ReadLine(), out decimal x3_);
+                    Console.Write("Y - ");
+                    bool isY3_ = decimal.TryParse(Console.ReadLine(), out decimal y3_);
+
+                    //Проверяю, что формат вводных данных правильный
+                    if (x1_ == 0 || x2_ == 0 || x3_ == 0 || y1_ == 0 || y2_ == 0 || y3_ == 0)
                     {
                         throw WrongNumberEx;
                     }
 
-                    double _res = 0;
-                    if (A == 0)
-                    {
-                        _res = 1;
-                    }
-                    else
-                    {
-                        for (ulong i = 0; i <= _n; i++)
-                        {
-                            _res += Math.Pow(A, i);
-                        }
-                    }
-
                     Console.ForegroundColor = ConsoleColor.Green;//меняю цвет текста
-                    Console.WriteLine($"Сумма - {_res}");
+                    Console.WriteLine($"Четверть первой точки = {MethodsForLab15.Quarter(x1_, y1_)}");
+                    Console.WriteLine($"Четверть второй точки = {MethodsForLab15.Quarter(x2_, y2_)}");
+                    Console.WriteLine($"Четверть третьей точки = {MethodsForLab15.Quarter(x3_, y3_)}");
                     Console.ResetColor(); // сбрасываю цвета консоли
 
                     Console.BackgroundColor = ConsoleColor.Blue;  //меняю цвет фона текста
@@ -147,27 +166,17 @@ namespace Lab_13
                 case ConsoleKey.D5:
 
                     Console.Write(" - задание \n");
-                    Console.Write("Введите число A - ");
-                    bool isA = double.TryParse(Console.ReadLine(), out double _A);
-                    Console.Write("Введите число N - ");
-                    bool is_N = ulong.TryParse(Console.ReadLine(), out ulong _N);
+                    Console.Write("Введите число для вычисления двойного факториала - ");
+                    bool isNumber = ulong.TryParse(Console.ReadLine(), out ulong number);
+
                     //вызываю собственные ошибки при нарушении правил ввода 
-                    if (is_N == false || isA == false)
+                    if (isNumber == false)
                     {
                         throw WrongNumberEx;
                     }
 
-                    double _result = 0;
-                    sbyte ratio = 1;
-                    for (ulong i = 0; i <= _N; i++)
-                    {
-                        _result += ratio * Math.Pow(_A, i);
-                        ratio *= -1;
-                    }
-
-
                     Console.ForegroundColor = ConsoleColor.Green;//меняю цвет текста
-                    Console.WriteLine($"Сумма - {_result}");
+                    Console.WriteLine($"Двойной факториал = {MethodsForLab15.Fact2(number)}");
                     Console.ResetColor(); // сбрасываю цвета консоли
 
                     Console.BackgroundColor = ConsoleColor.Blue;  //меняю цвет фона текста
